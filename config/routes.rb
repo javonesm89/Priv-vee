a950c447e60a76a30feeebaf39e4a5b57019d7e0
javonesm89 <javosmith12@gmail.com>
2021-01-27 14:07:55 -0500

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#home'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-  get '/logout' => 'sessionsdestroy'
+  get '/logout' => 'sessions#destroy'
   
   resources :users do
     resources :expenses
