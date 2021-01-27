4767b77bfd4ca6581ff9524639a231721cddbcbd
javonesm89 <javosmith12@gmail.com>
2021-01-27 14:36:18 -0500

    Update

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -1,6 +1,5 @@
 class IncomesController < ApplicationController
     def new
-        # binding.pry
         if params[:user_id] && @user = User.find_by_id(params[:user_id])
             @income = @user.incomes.build
         end
