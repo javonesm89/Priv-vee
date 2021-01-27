d0f376b6153d8981b224a617b48b5601f3d53a8e
javonesm89 <javosmith12@gmail.com>
2021-01-27 11:58:46 -0500

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+
+    def create
+        binding.pry
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name,:password,:email)
+    end
 end
