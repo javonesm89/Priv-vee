e3ececfa704f29c161d14f4aeb2b45ec3fd40bc2
javonesm89 <javosmith12@gmail.com>
2021-01-27 17:37:22 -0500

    Update expenses_controller.rb

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,2 +1,25 @@
 class ExpensesController < ApplicationController
+    def new
+        if params[:user_id] && @user = User.find_by_id(params[:user_id])
+            @expense = @user.expenses.build
+            @expenses = @user.expenses
+        end
+    end
+
+    def create
+        if session[:user_id] && User.find_by(:id => session[:user_id])
+            @expense = Expense.new(expenses_params)
+            if @expense.save
+                redirect_to user_path(@expense.user)
+            else
+                render :new
+            end
+        end
+    end
+
+    private
+
+    def expenses_params
+        params.require(:expense).permit(:source,:amount,:income_id,:user_id)
+    end
 end
