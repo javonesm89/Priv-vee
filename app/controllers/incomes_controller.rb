b277e3de1e180571bda67b208b04fbc8e0310d29
javonesm89 <javosmith12@gmail.com>
2021-01-27 15:04:55 -0500

    Update incomes_controller.rb

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -1,11 +1,12 @@
 class IncomesController < ApplicationController
     def new
         if params[:user_id] && @user = User.find_by_id(params[:user_id])
-            @income = Income.new(:user_id => @user.id)
+            @income = @user.incomes.build
         end
     end
 
     def create
+        @income = Income.new(income_params)
         binding.pry
     end
 
