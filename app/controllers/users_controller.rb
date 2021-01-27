599329c60e56f4107887451f41734f8d70b0ef90
javonesm89 <javosmith12@gmail.com>
2021-01-27 12:01:49 -0500

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,15 @@ class UsersController < ApplicationController
     end
 
     def create
-        binding.pry
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            binding.pry
+            redirect_to user_path(@user)
+        else
+            binding.pry
+            render :new
+        end
     end
 
     private
