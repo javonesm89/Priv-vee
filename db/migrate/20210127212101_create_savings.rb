9879bdd92cd003bc595a36548e97bc21f934beb1
javonesm89 <javosmith12@gmail.com>
2021-01-27 16:24:20 -0500

    Update 20210127212101_create_savings.rb

--- db/migrate/20210127212101_create_savings.rb
+++ db/migrate/20210127212101_create_savings.rb
@@ -3,7 +3,7 @@ class CreateSavings < ActiveRecord::Migration[6.0]
     create_table :savings do |u|
       u.string :source
       u.integer :amount
-      u.integer :off_percent
+      u.integer :off_percent, :default => 0
       u.references :user, null: false, foreign_key: true
       u.references :income, null: false, foreign_key: true
 
