c6bd9d71d95f8224ef55332694725b637253fcb4
javonesm89 <javosmith12@gmail.com>
2021-01-27 16:54:54 -0500

    Update

--- app/controllers/savings_controller.rb
+++ app/controllers/savings_controller.rb
@@ -2,7 +2,6 @@ class SavingsController < ApplicationController
     def new
         if params[:user_id] && @user = User.find_by_id(params[:user_id])
             @saving = @user.incomes.build
-            binding.pry
         end
     end
 
--- db/migrate/20210127212101_create_savings.rb
+++ db/migrate/20210127212101_create_savings.rb
@@ -3,7 +3,9 @@ class CreateSavings < ActiveRecord::Migration[6.0]
     create_table :savings do |u|
       u.string :source
       u.integer :amount
+      
       u.integer :off_percent, :default => 0
+
       u.references :user, null: false, foreign_key: true
       u.references :income, null: false, foreign_key: true
 
