de9a6def812add21332d17f94f937f57980e18a1
javonesm89 <javosmith12@gmail.com>
2021-01-27 18:08:03 -0500

    Update saving.rb

--- app/models/saving.rb
+++ app/models/saving.rb
@@ -1,4 +1,8 @@
 class Saving < ApplicationRecord
   belongs_to :user
   belongs_to :income
+
+  validates :source, :presence => true
+  validates :amount, :numericality => {:greater_than => 1}
+  validates :off_percent, :numericality => {:greater_than => 1}
 end
