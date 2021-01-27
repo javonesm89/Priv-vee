5019f72d231c45190bd29d8a6f6bab9f5dae9e18
javonesm89 <javosmith12@gmail.com>
2021-01-27 17:55:06 -0500

    Update expense.rb

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,10 @@
 class Expense < ApplicationRecord
   belongs_to :user
   belongs_to :income
+
+  validates :source, :presence => true
+  validates :amount, :numericality => {:greater_than => 1}
+  validates :off_percent, :numericality => {:greater_than => 1}
 end
+
+
