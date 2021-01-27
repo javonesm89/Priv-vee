924e2bb309f669fda5e1454af42abe5728e66889
javonesm89 <javosmith12@gmail.com>
2021-01-27 16:50:47 -0500

    Update

--- app/controllers/savings_controller.rb
+++ app/controllers/savings_controller.rb
@@ -2,6 +2,11 @@ class SavingsController < ApplicationController
     def new
         if params[:user_id] && @user = User.find_by_id(params[:user_id])
             @saving = @user.incomes.build
+            binding.pry
         end
+    end
+
+    def create
+        binding.pry
     end
 end
