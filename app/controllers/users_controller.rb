0a9a0d2cc189233cbfb2c3d7926956f6bdcc3c63
javonesm89 <javosmith12@gmail.com>
2021-01-27 12:04:37 -0500

    Update user show.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
-            binding.pry
             redirect_to user_path(@user)
         else
             binding.pry
