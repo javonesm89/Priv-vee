f4d3213087003d62a147dc55505aa56186276d81
javonesm89 <javosmith12@gmail.com>
2021-01-27 12:09:56 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  root 'welcome#home'
+  
+  resources :users do
+    resources :expenses
+    resources :savings
+    resources :incomes
+  end
+
   resources :expenses
   resources :savings
   resources :incomes
-  root 'welcome#home'
-  resources :users
+
 end
