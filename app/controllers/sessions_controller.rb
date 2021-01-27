1eb4f670221cdf9d63e353589f358d0be5c44903
javonesm89 <javosmith12@gmail.com>
2021-01-27 15:25:37 -0500

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,7 @@ class SessionsController < ApplicationController
     end
 
     def create
-        if params[:email] && @user = User.find_by_email(params[:email])
-            if @user.authenticate(params[:password])
-                session[:user_id] = @user.id
-                redirect_to user_path(@user)
-            else
-                flash[:alert] = 'UNABLE TO LOGIN. TRY AGAIN.'
-                redirect_to login_path
-            end
-        end
+
     end
 
     def destroy
