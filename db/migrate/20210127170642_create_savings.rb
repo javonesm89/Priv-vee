ab00c43cb4fd67b5c14d88e780897804df88b14f
javonesm89 <javosmith12@gmail.com>
2021-01-27 15:57:13 -0500

    Update table

--- db/migrate/20210127170642_create_savings.rb
+++ db/migrate/20210127170642_create_savings.rb
@@ -3,6 +3,7 @@ class CreateSavings < ActiveRecord::Migration[6.0]
     create_table :savings do |t|
       t.string :source
       t.string :amount
+      t.references :income, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
