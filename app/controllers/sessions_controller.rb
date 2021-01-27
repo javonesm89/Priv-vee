73fcafc3db2f42abe4a0f1c193eda8c2314b1df9
javonesm89 <javosmith12@gmail.com>
2021-01-27 12:47:46 -0500

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,13 @@ class SessionsController < ApplicationController
     end
 
     def create
+        if params[:email] && @user = User.find_by(:email => params[:email])
+            if @user.authenticate(params[:password])
+                redirect_to user_path(@user)
+            else
+                binding.pry
+                render :new
+            end
+        end
     end
 end
