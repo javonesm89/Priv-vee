aa2232d28ed1809302b215d31f6d613d8aa24afb
javonesm89 <javosmith12@gmail.com>
2021-01-28 18:03:02 -0500

    Update income.rb

--- app/models/income.rb
+++ app/models/income.rb
@@ -1,5 +1,7 @@
 class Income < ApplicationRecord
   belongs_to :user
   belongs_to :planner
-  
+
+  validates :source, :presence => true
+  validates :amount, :numericality => {:greater_than => 1}
 end
