369422ae347f8277e7a2d0cf35cac896e030d0da
javonesm89 <javosmith12@gmail.com>
2021-01-28 17:18:17 -0500

    Update

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -4,10 +4,6 @@ class Expense < ApplicationRecord
 
   validates :source, :presence => true
   validates :amount, :numericality => {:greater_than => 1}
-
-  def self.total_amount
-    total_amount = Expense.sum
-  end
 end
 
 
