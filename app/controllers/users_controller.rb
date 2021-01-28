ec3fbef85c8177328ab1f9da2dc0fbe7922382bf
javonesm89 <javosmith12@gmail.com>
2021-01-28 18:12:50 -0500

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
+            flash[:succes] = "CONGRATS ACCOUNT CREATED!"
             redirect_to user_path(@user)
         else
             @errors = @user.errors.full_messages
