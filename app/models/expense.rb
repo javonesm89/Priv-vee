a4c550a8d991b06cc864625aed340777ebea539e
javonesm89 <javosmith12@gmail.com>
2021-01-28 18:04:06 -0500

    Update

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -3,7 +3,7 @@ class Expense < ApplicationRecord
   belongs_to :planer
 
   validates :source, :presence => true
-  validates :amount, :numericality => {:greater_than => 1}
+  validates :amount, :numericality => {:greater_than_or_equal_to => 1}
 end
 
 
--- app/models/income.rb
+++ app/models/income.rb
@@ -3,5 +3,5 @@ class Income < ApplicationRecord
   belongs_to :planner
 
   validates :source, :presence => true
-  validates :amount, :numericality => {:greater_than => 1}
+  validates :amount, :numericality => {:greater_than_or_equal_to => 1}
 end
