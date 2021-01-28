ec468e351e8de6e0ed9ad43c051a3aec3c619dc2
javonesm89 <javosmith12@gmail.com>
2021-01-28 18:03:26 -0500

    Update saving.rb

--- app/models/saving.rb
+++ app/models/saving.rb
@@ -3,6 +3,5 @@ class Saving < ApplicationRecord
   belongs_to :planner
 
   validates :source, :presence => true
-  validates :amount, :numericality => {:greater_than_or_equal_to => 0}
-  validates :off_percent, :numericality => {:greater_than_or_equal_to => 0}
+  validates :amount, :numericality => {:greater_than_or_equal_to => 1}
 end
