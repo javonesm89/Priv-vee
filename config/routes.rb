3c718f410a0a332ed21041d73e50f749bffa25e9
javonesm89 <javosmith12@gmail.com>
2021-01-28 18:09:04 -0500

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
-  resources :planners
   root 'welcome#home'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
   
   resources :users do
+    resources :planners, :shallow => true
     resources :incomes, :shallow => true
     resources :expenses, :shallow => true
     resources :savings, :shallow => true
   end
-
+  
+  resources :planners
   resources :expenses
   resources :savings
   resources :incomes
