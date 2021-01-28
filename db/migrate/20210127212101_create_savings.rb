58e809811b24c46c67ee893b489d70ea0d293ae3
javonesm89 <javosmith12@gmail.com>
2021-01-28 17:54:21 -0500

    Deleted schema and update user show.erb

--- db/migrate/20210127212101_create_savings.rb
+++ db/migrate/20210127212101_create_savings.rb
@@ -2,10 +2,7 @@ class CreateSavings < ActiveRecord::Migration[6.0]
   def change
     create_table :savings do |u|
       u.string :source
-      u.integer :amount, :default => 0
-      
-      u.integer :off_percent, :default => 0
-
+      u.integer :amount
       u.references :user, null: false, foreign_key: true
       u.references :income, null: false, foreign_key: true
 
