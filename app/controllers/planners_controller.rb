89bbf10716b375b7dcf036332e42977634705b2f
javonesm89 <javosmith12@gmail.com>
2021-02-02 14:49:14 -0500

    Updates

--- app/controllers/planners_controller.rb
+++ app/controllers/planners_controller.rb
@@ -27,6 +27,6 @@ class PlannersController < ApplicationController
     private
 
     def planner_params
-        params.require(:planner).permit(:month,:user_id,:financial_goal)
+        params.require(:planner).permit(:month,:user_id,:financial_goal,:incomes_attributes => [:source,:amount])
     end
 end
