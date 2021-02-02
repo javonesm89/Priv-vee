0d0a2b4704fc65e09c63a75c7d9263587cdbdc5b
javonesm89 <javosmith12@gmail.com>
2021-02-02 13:00:05 -0500

    Update expense.rb

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,6 @@
 class Expense < ApplicationRecord
   belongs_to :user
-  belongs_to :planer
+  belongs_to :planner
 
   validates :source, :presence => true
   validates :amount, :numericality => {:greater_than_or_equal_to => 1}
