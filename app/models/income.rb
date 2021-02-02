133521db602314bc0a77a7aa59d569781ad06379
javonesm89 <javosmith12@gmail.com>
2021-02-02 11:35:33 -0500

    Update income.rb

--- app/models/income.rb
+++ app/models/income.rb
@@ -4,4 +4,6 @@ class Income < ApplicationRecord
 
   validates :source, :presence => true
   validates :amount, :numericality => {:greater_than_or_equal_to => 1}
+
+  accepts_nested_attributes_for :planner
 end
