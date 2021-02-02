e5b2a8ba0b91c84367caca9daf9b4ed6afbda342
javonesm89 <javosmith12@gmail.com>
2021-02-02 13:27:18 -0500

    Update 20210128225844_create_planners.rb

--- db/migrate/20210128225844_create_planners.rb
+++ db/migrate/20210128225844_create_planners.rb
@@ -3,6 +3,7 @@ class CreatePlanners < ActiveRecord::Migration[6.0]
     create_table :planners do |t|
       t.string :month
       t.text :financial_goal
+      t.boolean :goal_met => false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
