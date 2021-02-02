2cfc6a93e281dede83a4897a4d788be409efcb06
javonesm89 <javosmith12@gmail.com>
2021-02-02 15:42:16 -0500

    Updates

--- app/controllers/planners_controller.rb
+++ app/controllers/planners_controller.rb
@@ -27,6 +27,7 @@ class PlannersController < ApplicationController
 
     def update
         @planner = Planner.find_by(:id => params[:id])
+        binding.pry
         @planner.update(planner_params)
         if @planner.save
         end
