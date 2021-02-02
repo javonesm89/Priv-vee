178570645a020f0c1e19d5ff62317f39a7f7872e
javonesm89 <javosmith12@gmail.com>
2021-02-02 15:43:03 -0500

    Update planner.rb

--- app/models/planner.rb
+++ app/models/planner.rb
@@ -8,7 +8,5 @@ class Planner < ApplicationRecord
   validates :month, :presence => true
   validates :financial_goal, :presence => true
 
-  def incomes_attributes=(attributes)
-      self.incomes.build
-  end
+  accepts_nested_attributes_for :incomes
 end
