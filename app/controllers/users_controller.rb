64e46e8cd662bebcbeed3bfee08dbc36341b91ce
javonesm89 <javosmith12@gmail.com>
2021-02-02 10:41:21 -0500

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
     def show
-        binding.pry
         if params[:id] && @user = User.find_by(:id => session[:user_id])
                 @user 
         else
