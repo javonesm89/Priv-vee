f34f8c87523448ee63ff55d809b3055269190135
javonesm89 <javosmith12@gmail.com>
2021-02-02 10:31:11 -0500

    Update planners_controller.rb

--- app/controllers/planners_controller.rb
+++ app/controllers/planners_controller.rb
@@ -1,6 +1,5 @@
 class PlannersController < ApplicationController
     def new
-        # binding.pry
         if session[:user_id] && @user = User.find_by(:id => session[:user_id])
             @planner = @user.planners.build
         else
