e0bd37fb5444ca29c30a905488492fe4d322e59f
javonesm89 <javosmith12@gmail.com>
2021-02-10 16:44:55 -0500

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-    def display_dashboard
-        binding.pry
+    def display_savings
+        link_to "YTD Savings", user_savings_path(@user) 
     end
 
     def signup
